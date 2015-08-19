e5d452b1c604885b823025267249a85e4afdc9b6
Nate Willard <nate.a.willard@gmail.com>
2015-08-19 14:44:42 -0700

    Add functional-shared implementation of stack/queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,30 @@
 var Queue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var storage = {};
+  storage.numElements = 0;
+
+  _.extend(storage, queueMethods);
+  return storage;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value) {
+    this[this.numElements] = value;
+    this.numElements++;
+  },
+  dequeue: function() {
+    var result = this[0];
+    for (var i = 0; i < this.numElements-1; i++) {
+      this[i] = this[i+1];
+    }
+    this.numElements = (this.numElements === 0) ? 0 : this.numElements-1;
+    return result;
+  },
+  size: function() {
+    return this.numElements;
+  }
+};
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,25 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var storage = {};
+  storage.numElements = 0;
+
+  _.extend(storage, stackMethods);
+  return storage;
 };
 
-var stackMethods = {};
-
+var stackMethods = {
+  size: function() {
+    return this.numElements;
+  },
+  push: function(value) {
+    this[this.numElements] = value;
+    this.numElements++;
+  },
+  pop: function() {
+    this.numElements = (this.numElements === 0) ? 0 : this.numElements-1;
+    return this[this.numElements];
+  }
+};
 
