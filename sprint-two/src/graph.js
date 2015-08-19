5e378490a67eea05989613bc09d5bd8203b31de5
Nate Willard <nate.a.willard@gmail.com>
2015-08-19 16:17:34 -0700

    Add graph constructor, addNode, contains

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,19 +1,32 @@
-
+// NOTE: done in PSEUDOCLASSICAL style
+// Thoughts: we need some way in which we store these nodes
+// Option 1: we add the first node as the 'head' and then traverse edges.
+//  This is a bad idea, because then we cannot add a node without an edge. It will disappear
+// Option 2: We store all of these nodes in an array
+//  This could work
 
 // ###Graph Solution
 
 // Instantiate a new graph
 var Graph = function(){
+  // Remember: b/c this is called with the New keyword, this is now an empty object
+  this.nodes = [];
 };
 
 // ------------------------
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node){
+  this.nodes.push(node);
 };
 
 // ------------------------
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node){
+  debugger;
+  for (var i = 0; i < this.nodes.length; i++) {
+    if (this.nodes[i] === node) { return true; }
+  }
+  return false;
 };
 
 // ------------------------
