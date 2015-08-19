9062095a819e2de8849286ccca19580e656b59fa
Nate Willard <nate.a.willard@gmail.com>
2015-08-19 14:45:07 -0700

    Add prototypal stack/queue

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,27 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  
+  // key to prototypal: we won't extend, but we will create a separate object (as a prototype) which has the appropriate functions. Here that separate object will be stackMethods
+
+  var storage = Object.create(stackMethods);
+  storage.numElements = 0;
+
+  return storage;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value) {
+    this[this.numElements] = value;
+    this.numElements++;
+  },
+  pop: function() {
+    this.numElements = (this.numElements === 0 ) ? 0 : this.numElements-1;
+    return this[this.numElements];
+  },
+  size: function() {
+    return this.numElements;
+  }
+};
 
 
