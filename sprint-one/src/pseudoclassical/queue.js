7b2bd79ce8fa65934c68879a06329f1a4959115c
Nate Willard <nate.a.willard@gmail.com>
2015-08-19 14:56:09 -0700

    Add pseudoclassical stack/queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,24 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.numElements = 0;
+};
+
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.numElements] = value;
+  this.numElements++;
+};
+
+Queue.prototype.dequeue = function() {
+  var result = this.storage[0];
+  for (var i = 0; i < this.numElements-1; i++) {
+    this.storage[i] = this.storage[i+1];
+  }
+  this.numElements = Math.max(this.numElements-1,0);
+  return result;
+};
+
+Queue.prototype.size = function() {
+  return this.numElements;
 };
 
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,22 @@
+// This function is the CONSTRUCTOR
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.storage = {}; 
+  // (note that we CANNOT SIMPLY do this = {}, so we have to modify the other elements to operate on this.storage[] instead of simply this[]
+  this.numElements = 0;
 };
 
+Stack.prototype.push = function(value) {
+  var storage = this.storage;
+  storage[this.numElements] = value;
+  this.numElements++;
+};
+
+Stack.prototype.pop = function() {
+  this.numElements = Math.max(this.numElements-1, 0);
+  return this.storage[this.numElements];
+};
+
+Stack.prototype.size = function() {
+  return this.numElements;
+};
 
