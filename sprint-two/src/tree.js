1d384a87686bc7a276a96571be5bf9aee3943056
Nate Willard <nate.a.willard@gmail.com>
2015-08-19 16:01:35 -0700

    Add tree data structure

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,25 +2,27 @@ var Tree = function(value){
   var newTree = {};
   newTree.value = value;
 
-  // your code here
-  newTree.children = null;  // fix me
+  // NOTE: because we're using treeMethods, we're going to either have to use functional-shared (extend), or prototypal (object.create); Functional-shared will play better with the provided above code
+  newTree.children = [];
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
 
-
-  // your code here
-  newTree.children = null;  // fix me
-
-
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  var child = new Tree(value);
+  this.children.push(child);
 };
 
 treeMethods.contains = function(target){
-
+  debugger;
+  if (target === this.value) { return true; }
+  for (var i = 0; i < this.children.length; i++) {
+    if (this.children[i].contains(target)) { return true; }
+  }
+  return false; 
 };
 
 
