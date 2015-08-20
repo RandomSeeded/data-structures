75a5a1091b6a51c7216b0e4145606d498488fc8b
Nate Willard <nate.a.willard@gmail.com>
2015-08-20 10:03:52 -0700

    Add implementation of hash insert & retrieve

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,15 +5,29 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(i) || [];
+  var replacement = false;
+  /*for (var i = 0; i < bucket.length; i++) {
+    if (bucket[i].key === k) { 
+      bucket[i].value = v;
+      replaced = true;
+    }
+  }*/
+  if (!replacement) { bucket.push({key: k, value: v}); }
+  this._storage.set(i, bucket);
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
+  var bucket = this._storage.get(i);
+  for (var i = 0; i < bucket.length; i++) {
+    if (bucket[i].key === k) { return bucket[i].value; }
+  }
+  return null;
 };
 
 HashTable.prototype.remove = function(k){
-
+  //debugger;
 };
 
 
