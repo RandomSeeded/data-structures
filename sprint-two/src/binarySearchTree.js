fffaef24317a5df73210c3ac7dc67d0bdc7f0a1d
Nate Willard <nate.a.willard@gmail.com>
2015-08-20 11:44:42 -0700

    Saving binary search tree progress

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,32 @@
+// NOTE: not called with NEW keyword in provided fixtures, so we cannot pseudoclassical.
+// Prototypal used instead
 var BinarySearchTree = function(value){
+  var tree = Object.create(BinarySearchTree.prototype);
+  tree.left = null;
+  tree.right = null;
+  tree.value = value;
+  return tree;
+};
+
+BinarySearchTree.prototype.insert = function(value) {
+  // Recursive cases: keep looking down the tree
+  if (value < this.value && this.left !== null) { this.left.insert(value); }
+  else if (value > this.value && this.right !== null) { this.right.insert(value); }
+  // Base case: cannot go any further down the tree
+  else {
+    var newTree = new BinarySearchTree(value);
+    if (value < this.value) { this.left = newTree; }
+    else { this.right = newTree; }
+  }
+};
+
+BinarySearchTree.prototype.contains = function(value) {
+  if (value === this.value) { return true; }
+  else if (value < this.value && this.left !== null) { return this.left.contains(value); }
+  else if (value > this.value && this.right !== null) { return this.right.contains(value); }
+};
+
+BinarySearchTree.prototype.depthFirstLog = function() {
 };
 
 
