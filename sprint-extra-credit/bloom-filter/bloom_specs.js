73282ddb108cd63215b4a3f830f98e59de2d85bd
Nate Willard <nate.a.willard@gmail.com>
2015-08-21 11:16:34 -0700

    Add bloom filter tests

--- sprint-extra-credit/bloom-filter/bloom_specs.js
+++ sprint-extra-credit/bloom-filter/bloom_specs.js
@@ -1,9 +1,45 @@
-  var myBloom = new BloomFilter(18, 3);
-  myBloom.add('kitties');
-  myBloom.add('puppies');
-  myBloom.add('flowers');
-  myBloom.add('skulls');
-  console.log(myBloom.lookup('kitties'));
-  console.log(myBloom.lookup('bones'));
-  console.log(myBloom.lookup('dogs'));
-  console.log(myBloom.lookup('cats')); // FALSE POSITIVE
+describe('test suite', function() {
+  it('should have a working test suite', function() {
+    expect(true).to.equal(true);
+  });
+});
+describe('bloom_filter', function() {
+  var bloomFilter;
+  beforeEach(function() {
+    bloomFilter = new BloomFilter(18, 3);
+  });
+
+  it('should have a bloom filter', function() {
+    expect(bloomFilter).to.be.an('object');
+  });
+
+  debugger;
+  it('should have functions add and lookup', function() {
+    expect(bloomFilter.add).to.be.a('function');
+  });
+
+  it('should be able to able to add and lookup items', function() {
+    bloomFilter.add('kitties');
+    expect(bloomFilter.lookup('kitties')).to.equal(true);
+  });
+
+  it('should sometimes return false positives', function() {
+    var found = 0;
+    bloomFilter.add('kitties');
+    bloomFilter.add('puppies');
+    bloomFilter.add('flowers');
+    bloomFilter.add('skulls');
+    found += bloomFilter.lookup('cats');
+    found += bloomFilter.lookup('dogs');
+    found += bloomFilter.lookup('kangaroos');
+    expect(found).to.be.above(0);
+    expect(found).to.be.below(3);
+  });
+
+  it('should have a reasonable false positive rate', function() {
+    const numTrials = 100;
+    for (var trial = 0; i < numTrials; i++) {
+    }
+  }
+
+});
