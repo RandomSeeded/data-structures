1f36d6e7ac9697a94159b33ffe16d192437ff0f1
Nate Willard <nate.a.willard@gmail.com>
2015-08-22 10:28:43 -0700

    Add two cases rbt insert

--- sprint-extra-credit/red-black-tree/red_black_specs.js
+++ sprint-extra-credit/red-black-tree/red_black_specs.js
@@ -5,8 +5,7 @@ describe('test_suite', function() {
   var tree;
   beforeEach(function() {
     tree = new RedBlackTree(18, 0, null);
-    //bst = new BinarySearchTree(5);
-    debugger;
+    bst = new BinarySearchTree(5);
   });
 
   it('should inherit methods contains and depthFirstLog', function() {
@@ -24,6 +23,27 @@ describe('test_suite', function() {
 
   it('should correctly add an initial node', function() {
     expect(tree.contains(18)).to.equal(true);
+    expect(tree.contains(14)).to.equal(false);
   });
+
+  it('should correctly add nodes with fathers who are black', function() {
+    tree.insert(12);
+    tree.insert(24);
+    expect(tree.contains(12)).to.equal(true);
+    expect(tree.contains(24)).to.equal(true);
+    expect(tree.contains(25)).to.equal(false);
+  });
+
+  /*it('should correctly add balanced leaves', function() {
+    tree.insert(12);
+    tree.insert(24);
+    tree.insert(9);
+    tree.insert(15);
+    tree.insert(21);
+    tree.insert(27);
+    expect(tree.contains(12)).to.equal(true);
+    expect(tree.contains(21)).to.equal(true);
+    expect(tree.contains(28)).to.equal(false);
+  });*/
 
 });
--- sprint-extra-credit/red-black-tree/red_black_tree.js
+++ sprint-extra-credit/red-black-tree/red_black_tree.js
@@ -5,9 +5,11 @@ var _root;
 function RedBlackTree(value, color, father) {
   this.color = color;
   this.father = father;
+  // CASE 1: current node is at the root of the tree
   if (father === null) { _root = this; }
-  tree.left = null;
-  tree.right = null;
+  this.left = null;
+  this.right = null;
+  this.value = value;
 }
 
 RedBlackTree.prototype = Object.create(BinarySearchTree.prototype);
@@ -18,5 +20,18 @@ RedBlackTree.prototype.getRoot = function() {
 
 // Insert is overwritten
 RedBlackTree.prototype.insert = function(value) {
+  if (value < this.value && this.left !== null) { this.left.insert(value); }
+  else if (value > this.value && this.right !== null) { this.right.insert(value); }
+  else {
+    debugger;
+    var newTree = new RedBlackTree(value, 1, this);
+    // CASE 2: the node to be inserted's parent is BLACK
+    if (newTree.father && newTree.father.color !== 1) {
+      if (value < this.value) { this.left = newTree; }
+      else { this.right = newTree; }
+    }
+
+    // CASE 3: both the PARENT and GRANDPARENT are RED
+  }
 };
 
