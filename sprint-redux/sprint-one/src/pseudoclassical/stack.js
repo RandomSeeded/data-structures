c613edd8f502bc6707dffafaecc5ca9550a8cef9
Nate Willard <nate.a.willard@gmail.com>
2015-08-23 08:52:00 -0700

    Add pseudoclassical stack

--- sprint-redux/sprint-one/src/pseudoclassical/stack.js
+++ sprint-redux/sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,21 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this._size = 0;
 };
+
+Stack.prototype.push = function(value) {
+  this[this._size++] = value;
+}
+
+Stack.prototype.pop = function() {
+  if (this._size === 0) { return null; }
+  this._size--;
+  return this[this._size];
+}
+
+Stack.prototype.size = function() {
+  return this._size;
+}
 
 
