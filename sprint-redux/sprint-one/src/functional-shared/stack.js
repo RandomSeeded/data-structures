e230880bdb6173dbe43e90bb82e157c65c46f0b8
Nate Willard <nate.a.willard@gmail.com>
2015-08-23 08:41:34 -0700

    Add func shared stack

--- sprint-redux/sprint-one/src/functional-shared/stack.js
+++ sprint-redux/sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,24 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var storage = {};
+  storage._size = 0;
+  _.extend(storage, stackMethods);
+  return storage;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value) {
+    this[this._size++] = value;
+  },
+  pop: function() {
+    if (this._size === 0) { return null; }
+    this._size--;
+    return this[this._size];
+  },
+  size: function() {
+    return this._size;
+  }
+};
 
 
