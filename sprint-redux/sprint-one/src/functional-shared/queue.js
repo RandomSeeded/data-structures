da550545f731f4980001c1e5273626f2f7985465
Nate Willard <nate.a.willard@gmail.com>
2015-08-23 08:36:15 -0700

    Add func shared queue

--- sprint-redux/sprint-one/src/functional-shared/queue.js
+++ sprint-redux/sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,23 @@
 var Queue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var storage = {};
+  storage.first = 0;
+  storage.last = 0;
+  _.extend(storage, queueMethods);
+  return storage;
 };
 
-var queueMethods = {};
-
+var queueMethods = {
+  enqueue: function(value) {
+    this[this.last++] = value;
+  },
+  dequeue: function() {
+    if (this.size() === 0) { return null; }
+    return this[this.first++];
+  },
+  size: function() {
+    return this.last - this.first;
+  }
+};
 
