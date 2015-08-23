51a0b712b6a59f5ca5c33af98c87e39ec77e51af
Nate Willard <nate.a.willard@gmail.com>
2015-08-23 08:51:51 -0700

    Add pseudoclassical queue

--- sprint-redux/sprint-one/src/pseudoclassical/queue.js
+++ sprint-redux/sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,21 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this._first = 0;
+  this._last = 0;
 };
+
+Queue.prototype.enqueue = function(value) {
+  this[this._last++] = value;
+}
+
+Queue.prototype.dequeue = function(value) {
+  if (this.size() === 0) { return null; }
+  return this[this._first++];
+}
+
+Queue.prototype.size = function(value) {
+  return this._last - this._first;
+}
 
 
